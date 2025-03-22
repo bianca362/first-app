2893965b5b27d452bf2476d7eff3ee50bbc49845
bianca362 <nestbianca22@gmail.com>
2025-03-23 00:32:35 +0100

    Added root route

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post("/recommend",(req,res)=>{
     res.json(recommendations[mood]);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.get("/",(req,res)=>{
     res.send("welcome to the mood-based recommendation API");
